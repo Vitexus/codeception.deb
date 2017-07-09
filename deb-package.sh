478490f3408ecd04ae1d4283c0934c75e7e44e6e
Vítězslav Dvořák <info@vitexsoftware.cz>
2017-07-09 10:58:38 +0200

    Package build scripts update

--- deb-package.sh
+++ deb-package.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 
-./make-phar.sh
-
-BINARY=`ls codeception/codeception-*.phar`
-VERSION=`echo $BINARY | sed -e 's/codeception\/codeception-v//g' | sed -e 's/.phar//g'`
-echo version: $VERSION
-mv $BINARY codeception/codeception
+./fresh.sh
 
 PACKAGE="php-codeception"
-REVISION=`cat debian/revison | perl -ne 'chomp; print join(".", splice(@{[split/\./,$_]}, 0, -1), map {++$_} pop @{[split/\./,$_]}), "\n";'`
-
-cd codeception
+VERSION=`cat Codeception/src/Codeception/Codecept.php | grep "const VERSION" | awk -F'"' '{print $2}' | tr -d '\n'`
+REVISION=`cat debian/revision | perl -ne 'chomp; print join(".", splice(@{[split/\./,$_]}, 0, -1), map {++$_} pop @{[split/\./,$_]}), "\n";'`
+echo Build now version: $VERSION-$REVISION
+sleep 2
+cd Codeception
 CHANGES=`git log -n 1 | tail -n+5`
 cd ..
 
@@ -23,9 +20,9 @@ debuild -i -us -uc -b
 LASTVERSION=`cat debian/lastversion`
 if [ $LASTVERSION == $VERSION  ];
 then
-    echo $REVISION > debian/revison
+    echo $REVISION > debian/revision
 else
-    echo 0 > debian/revison
+    echo 0 > debian/revision
     echo $VERSION > debian/lastversion
 fi
 
--- prepack.sh
+++ prepack.sh
@@ -4,5 +4,5 @@ cp -f Codeception/codecept-*.phar debian/php-codeception/usr/bin/codecept
 chmod +x debian/php-codeception/usr/bin/codecept
 mkdir -p debian/php-codeception/usr/share/php
 cp -rf Codeception/src/* debian/php-codeception/usr/share/php
-
+chmod -x debian/php-codeception/usr/share/php/Codeception/Lib/Driver/Db.php
 
